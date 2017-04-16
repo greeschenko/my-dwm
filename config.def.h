2f6894d4832e27606f668a501b089d827afbe905
Oleksiy Hryshchenko <greeschenko@gmail.com>
2017-04-16 23:38:06 +0000

    up

--- config.def.h
+++ config.def.h
@@ -2,26 +2,26 @@
 
 /* appearance */
 static const char *fonts[] = {
-	"monospace:size=9"
+    "monospace:size=9",
+    "FontAwesome:size=9"
 };
-static const char dmenufont[]            = "monospace:size=16";
-static const char normbordercolor[]      = "#444444";
-static const char normbgcolor[]          = "#444444";
-static const char normfgcolor[]          = "#CCC2C2";
-static const char selbordercolor[]       = "#ff575f";
-static const char selbgcolor[]           = "#444444";
-static const char selfgcolor[]           = "#ff575f";
-static const unsigned int borderpx       = 1;        /* border pixel of windows */
-static const unsigned int snap           = 15;       /* snap pixel */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, 0: display systray on the last monitor*/
-static const int showsystray             = 1;        /* 0 means no systray */
-static const int showbar                 = 1;        /* 0 means no bar */
-static const int topbar                  = 1;        /* 0 means bottom bar */
+static const char dmenufont[]       = "monospace:size=16";
+static const char normbordercolor[] = "#444444";
+static const char normbgcolor[]     = "#222222";
+static const char normfgcolor[]     = "#bbbbbb";
+static const char selbordercolor[]  = "#005577";
+static const char selbgcolor[]      = "#005577";
+static const char selfgcolor[]      = "#eeeeee";
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int snap      = 15;       /* snap pixel */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+static const char *tags[] = {
+    "1:task", "2:work", "3:web", "4:sys",
+    "5:free", "6:gimp", "7:ink", "8:vbox", "9:ff"
+};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -37,14 +37,14 @@ static const Rule rules[] = {
 
 #define NUMCOLORS         4
 static const char colors[NUMCOLORS][MAXCOLORS][8] = {
-	// border   foreground background
-	{ "#444444", "#CCC2C2", "#444444" },  // normal
-	{ "#ff575f", "#ff575f", "#444444" },  // selected
-	{ "#FF8C00", "#cccccc", "#FF8C00" },  // urgent/warning  (black on yellow)
-	{ "#ff575f", "#ffffff", "#ff575f" },  // error (white on red)
+    // border   foreground background
+    { "#444444", "#CCC2C2", "#444444" },  // normal
+    { "#ff575f", "#ff575f", "#444444" },  // selected
+    { "#FF8C00", "#cccccc", "#FF8C00" },  // urgent/warning  (black on yellow)
+    { "#ff575f", "#ffffff", "#ff575f" },  // error (white on red)
     { "#2972CC", "#ffffff", "#2972CC" },  // primary
     { "#83CC29", "#ffffff", "#83CC29" },  // success
-	// add more here
+    // add more here
 };
 
 /* layout(s) */
@@ -55,10 +55,8 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
-	{ "TTT",      bstack },
-	{ "===",      bstackhoriz },
-	{ "|M|",      centeredmaster },
-	{ ">M>",      centeredfloatingmaster },
+	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[M]",      monocle },
 };
 
 /* key definitions */
@@ -74,8 +72,11 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-w", "904", "-x", "226", "-y", "300", NULL };
-
+static const char *dmenucmd[] = {
+    "dmenu_run", "-fn", dmenufont, "-nb",
+    normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-w", "904",
+    "-x", "226", "-y", "300", NULL
+};
 static const char *termcmd[]     = { "xterm", NULL };
 static const char *vimcmd[]      = { "xterm", "-e", "vim", NULL };
 static const char *mccmd[]       = { "xterm", "-e", "mc", NULL };
@@ -123,29 +124,26 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[3]} },
-	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_s,      togglesticky,   {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-    { 0,                            0x1008ff13,   spawn,		   {.v = apluscmd } },
-    { 0,                            0x1008ff11,   spawn,		   {.v = aminuscmd } },
-    { 0,                            0x1008ff12,          spawn,		   {.v = atogglecmd } },
-    { 0,                            0x1008ff2f,          spawn,		   {.v = sleepcmd } },
-    { MODKEY,                       0x1008ff2f,          spawn,		   {.v = shutdowncmd } },
-    { 0,                            0x1008ff59,          spawn,		   {.v = screencmd } },
-    { 0,                            0x1008ff03,          spawn,		   {.v = ldowncmd } },
-    { 0,                            0x1008ff02,          spawn,		   {.v = lupcmd } },
-    { 0,				            XK_Print,  		    spawn,          {.v = scrotcmd } },
-    { ControlMask,			        XK_4,  	    spawn,          {.v = setenkb } },
-    { ControlMask,			        XK_3,  	    spawn,          {.v = setrukb } },
-    { ControlMask,			        XK_2,  	    spawn,          {.v = setuakb } },
+    { 0,                            0x1008ff13,   spawn,	   {.v = apluscmd } },
+    { 0,                            0x1008ff11,   spawn,	   {.v = aminuscmd } },
+    { 0,                            0x1008ff12,   spawn,	   {.v = atogglecmd } },
+    { 0,                            0x1008ff2f,   spawn,	   {.v = sleepcmd } },
+    { MODKEY,                       0x1008ff2f,   spawn,	   {.v = shutdowncmd } },
+    { 0,                            0x1008ff59,   spawn,	   {.v = screencmd } },
+    { 0,                            0x1008ff03,   spawn,	   {.v = ldowncmd } },
+    { 0,                            0x1008ff02,   spawn,	   {.v = lupcmd } },
+    { 0,				            XK_Print,  	  spawn,       {.v = scrotcmd } },
+    { ControlMask,			        XK_4,  	    spawn,         {.v = setenkb } },
+    { ControlMask,			        XK_3,  	    spawn,         {.v = setrukb } },
+    { ControlMask,			        XK_2,  	    spawn,         {.v = setuakb } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
